d3e641056f2e3a005580514c4a291309d3d985a2
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-10 21:36:46 +0300

    gh-4 Квадратичная сложность логики

--- task_001.py
+++ task_001.py
@@ -1,5 +1,8 @@
 #Counting DNA Nucleotides http://rosalind.info/problems/dna/
-file = open("dna.txt", "r")
-dna_text = file.read()
-letters = dict(zip(list(dna_text),[list(dna_text).count(i) for i in list(dna_text)]))
-print(letters['A'],' ',letters['C'], ' ', letters['G'], ' ', letters['T'])
+import collections
+
+with open("dna.txt", "r") as f:
+    dna_text = f.read()
+
+counter = collections.Counter(dna_text)
+print(counter['A'], ' ', counter['C'], ' ', counter['G'], ' ', counter['T'])
