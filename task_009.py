1147ebe52463c9522353dcd7c5c8bd501241382b
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-11 14:11:21 +0300

    gh-13 Ошибка

--- task_009.py
+++ task_009.py
@@ -1,12 +1,13 @@
 #Finding a Motif in DNA http://rosalind.info/problems/subs/
-file = open("rosalind_subs.txt", "r")
-s, t = file.read().split()
-answer = list()
-index = 0
+with open("rosalind_subs.txt", "r") as f:
+    s, t = f.read().split()
+
+answer = []
+index = s.find(t)
 
 while index != -1:
     answer.append(index)
     index = s.find(t, (index+1))
- 
-for value in answer[1:]:
+        
+for value in answer:
     print(value+1, end=' ')
