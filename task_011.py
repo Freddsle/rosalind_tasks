21b9eb9f29ddfc5c5098ff98b46878945b209ab8
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-11 20:10:35 +0300

    store less values

--- task_011.py
+++ task_011.py
@@ -3,16 +3,16 @@ with open("rosalind_fibd.txt", "r") as f:
     n, m = (int(value) for value in f.read().split())
 
 #n, m = 6, 3
-rabbits = [[0]*n for i in range(m)]
-rabbits[0][0], rabbits[1][1] = 1, 1
+rabbits = [[0, 0, 0] for i in range(m)]
+rabbits[0][1], rabbits[1][2] = 1, 1
 
 for i in range(2, n):
     for j in range(m):
+        rabbits[j][0], rabbits[j][1] = rabbits[j][1], rabbits[j][2]
         if j == 0:
-            rabbits[j][i] = sum(rabbits[k][i-1] for k in range(1,m))
+            rabbits[0][2] = sum(rabbits[k][2] for k in range(1, m))
         else:
-            rabbits[j][i] = rabbits[j-1][i-1]
+            rabbits[j][2] = rabbits[j-1][1]
 
-stop_rab = sum(rabbits[i][n-1] for i in range(m))
+stop_rab = sum(rabbits[i][2] for i in range(m))
 print(stop_rab)
-
