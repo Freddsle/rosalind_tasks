1c037b67de3a819a6605d49bf17fe917d7737a3b
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-11 13:35:29 +0300

    gh-10 По возможности, данные из внешних источников следует обрабатывать итеративно

--- task_005.py
+++ task_005.py
@@ -1,13 +1,20 @@
 # Computing GC Content http://rosalind.info/problems/gc/
+import re
+
+seq, GC = {}, {}
+
 with open("rosalind_gc.txt", "r") as f:
-    line = f.read().strip('>').split('>')
+    for line in f:
+        line = line.strip('\n') 
+        if re.match(r'>Rosalind_\d{4}', line):
+            name = line
+            seq[name] = [0, 0]               # [len line, GC in line]
+        else:
+            seq[name][0] += len(line)
+            seq[name][1] += line.count('C') + line.count('G')
 
-GC = {}
-
-for i in line:
-    name, seq = i.split('\n', 1)
-    seq = seq.replace('\n', '')
-    GC[name] = ((seq.count('C') + seq.count('G')) / len(seq))
+for element in seq:
+    GC[element] = seq[element][1] / seq[element][0]
 
 name_max, GC_value = max(GC.items(), key = lambda k : k[1])
-print('{}\n{}'.format(name_max,GC_value*100))
+print('{}\n{}'.format(name_max, GC_value * 100))
