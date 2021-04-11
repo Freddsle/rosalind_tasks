60876d0bc8f9195237e26724176eab50b4fc916d
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-11 12:13:34 +0300

    gh-10 По возможности, данные из внешних источников следует обрабатывать итеративно

--- task_010.py
+++ task_010.py
@@ -1,36 +1,36 @@
 #Consensus and Profile http://rosalind.info/problems/cons/
-with open("rosalind_cons.txt", "r") as f:
-    line = f.read().strip('>').split('>')
+import re
 
-data = {}
-
-for i in line:
-    name, seq = i.split('\n', 1)
-    seq = seq.replace('\n', '')
-    data[name] = seq
-    ls = len(seq)
-
-Profile = {'A': [0] * ls,
-           'C': [0] * ls,
-           'G': [0] * ls,
-           'T': [0] * ls}
-
-for seq in data:
-    for i, c in enumerate(data[seq]):
-        Profile[c][i] += 1
-
-alphabet = ['A', 'C', 'G', 'T']
 answer = ''
+kb = 1000
+alphabet = ['A', 'C', 'G', 'T']
+Profile = {'A': [0] * kb,
+           'C': [0] * kb,
+           'G': [0] * kb,
+           'T': [0] * kb}
 
-for i in range(len(Profile['A'])):
-    l = (Profile['A'][i],Profile['C'][i],Profile['G'][i], Profile['T'][i])
-    index = l.index(max(l))
-    answer += alphabet[index]
+with open("rosalind_cons.txt", "r") as f:
+    for line in f:
+        if re.match(r'>Rosalind_\d{4}', line):
+            j = 0
+        else:
+            for i, c in enumerate(line.strip('\n')):
+                Profile[c][i+j] += 1
+            j += len(line) - 1
+            
+for i in range(kb):
+    l = (Profile['A'][i], 
+         Profile['C'][i],
+         Profile['G'][i], 
+         Profile['T'][i])
+    if sum(l) != 0:
+        index = l.index(max(l))
+        answer += alphabet[index]
 
 with open("myfile.txt", "w") as f:
-    print(answer, 
-        '\nA: ', *Profile['A'], 
-        '\nC: ', *Profile['C'], 
-        '\nG: ', *Profile['G'], 
-        '\nT: ', *Profile['T'], 
+    print(answer,
+        '\nA: ', *Profile['A'][:j],
+        '\nC: ', *Profile['C'][:j], 
+        '\nG: ', *Profile['G'][:j], 
+        '\nT: ', *Profile['T'][:j], 
         file=f)
