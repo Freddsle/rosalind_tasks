fcaed5c0add4adfb2ef3f102b44ce915a408b688
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-17 19:24:27 +0300

    gh-24 Можно проще

--- task_014.py
+++ task_014.py
@@ -8,35 +8,26 @@ def make_subs(last_position):
         substrings.append([i, last_position - i])
     return substrings
 
-
 def find_longes_prefix(template_string, string):
-    for i, c in enumerate(template_string):
-        if i == len(string):
+    for i, (letter_tem, letter_str) in enumerate(zip(template_string, string)):
+        if letter_tem != letter_str:
             return i
-        if i == len(template_string) - 1 and c == string[i]:
-            return i + 1
-        if i >= len(string):
-            return i
-        if i == len(string) and c == string[i]:
-            return i + 1
-        if c == string[i]:
-            continue
-        if c != string[i]:
-            return i
-    raise NotImplementedError
-  
+        else:
+            continue       
+    return i + 1
 
+  
 records = list(SeqIO.parse("rosalind_lcsm.txt", "fasta"))
 sequences = sorted([record.seq for record in records], key=len)
 start = make_subs(len(sequences[0]))
 
 for string in sequences[1:]:
     subs_pair = len(string)
-    for c, (begin, lenght) in enumerate(start):
+    for c, (begin, length) in enumerate(start):
         last_pos = []
         for pair in range(subs_pair):
-            last_pos.append(find_longes_prefix(sequences[0][begin:begin+lenght], string[pair:]))
-            if last_pos[pair] == len(sequences[0][begin:lenght+begin]):
+            last_pos.append(find_longes_prefix(sequences[0][begin:begin+length], string[pair:]))
+            if last_pos[pair] == len(sequences[0][begin:length+begin]):
                 break    
         start[c][1] = max(last_pos)
 
