ab102fa343d0e68c55a28b4929476a2af5b72364
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-17 10:48:10 +0300

    gh-24 Можно проще

--- task_014.py
+++ task_014.py
@@ -15,33 +15,32 @@ def find_longes_prefix(template_string, string):
     for i, c in enumerate(template_string):
         if i == len(string):
             return i
-        elif i == len(template_string) - 1 and c == string[i]:
+        if i == len(template_string) - 1 and c == string[i]:
             return i + 1
-        elif i >= len(string):
+        if i >= len(string):
             return i
-        elif i == len(string) and c == string[i]:
+        if i == len(string) and c == string[i]:
             return i + 1
-        elif c == string[i]:
+        if c == string[i]:
             continue
-        elif c != string[i]:
+        if c != string[i]:
             return i
-    
+    raise NotImplementedError
+  
 
-#records = list(SeqIO.parse("rosalind_lcsm.txt", "fasta"))
-records = list(SeqIO.parse("dna.txt", "fasta"))
+records = list(SeqIO.parse("rosalind_lcsm.txt", "fasta"))
 sequences = sorted([record.seq for record in records], key=len)
 start = make_subs(sequences[0], key='start')
 
 for string in sequences[1:]:
     subs_pair = len(string)
-    for element in start:
-        i, j = element[0], element[1]
+    for c, (i, j) in enumerate(start):
         last_pos = []
         for pair in range(subs_pair):
             last_pos.append(find_longes_prefix(sequences[0][i:i+j], string[pair:]))
             if last_pos[pair] == len(sequences[0][i:j+i]):
                 break    
-        element[1] = max(last_pos)
+        start[c][1] = max(last_pos)
 
 position = max(start, key=lambda x: x[1])
 print(sequences[0][position[0]:position[0]+position[1]])
