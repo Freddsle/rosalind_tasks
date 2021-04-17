30361c9d3e5607e7c7c1f2ada7aed9adc37f037d
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-04-17 10:24:09 +0300

    gh-26 Вместо индекса можно итерировать сразу элементы последовательности

--- task_014.py
+++ task_014.py
@@ -27,20 +27,21 @@ def find_longes_prefix(template_string, string):
             return i
     
 
-records = list(SeqIO.parse("rosalind_lcsm.txt", "fasta"))
-sequences = sorted([records[i].seq for i in range(len(records))], key=len)
+#records = list(SeqIO.parse("rosalind_lcsm.txt", "fasta"))
+records = list(SeqIO.parse("dna.txt", "fasta"))
+sequences = sorted([record.seq for record in records], key=len)
 start = make_subs(sequences[0], key='start')
 
 for string in sequences[1:]:
     subs_pair = len(string)
-    for k in range(len(start)):
-        i, j = start[k][0], start[k][1]
+    for element in start:
+        i, j = element[0], element[1]
         last_pos = []
         for pair in range(subs_pair):
             last_pos.append(find_longes_prefix(sequences[0][i:i+j], string[pair:]))
             if last_pos[pair] == len(sequences[0][i:j+i]):
                 break    
-        start[k][1] = max(last_pos)
+        element[1] = max(last_pos)
 
 position = max(start, key=lambda x: x[1])
 print(sequences[0][position[0]:position[0]+position[1]])
