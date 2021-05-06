8ff2ea179d613d3c6ea9b35c9dec4227d6c05694
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-05-06 17:35:03 +0300

    gh-33 Структурные блоки кода лучше визуально выделять

--- task_016/solution016.py
+++ task_016/solution016.py
@@ -18,12 +18,16 @@ def find_glycosylation_motif(sequence, MOTIF_RE):
 
 def main():
     # accessions = ['A2Z669', 'B5ZC00', 'P07204_TRBM_HUMAN', 'P20840_SAG1_YEAST']
+
     with open("rosalind_mprt.txt", "r") as f:
         accessions = f.read().split()
+
     records = download_sequences(accessions)
     MOTIF_RE = re.compile(r'(?=(N[^P][S,T][^P]))')
+
     for seq_id in records:
         result = find_glycosylation_motif(records[seq_id], MOTIF_RE)
+        
         if result != []:
             print(seq_id)
             print(*[x+1 for x in result])
