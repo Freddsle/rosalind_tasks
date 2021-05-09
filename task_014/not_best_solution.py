db131a1de94e8aff707d1a5ca17bcf484de35352
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-05-09 16:45:17 +0300

    gh-31 now - best solution

--- task_014/not_best_solution.py
+++ task_014/not_best_solution.py
@@ -1,3 +1,4 @@
+#now - BEST
 #Finding a Shared Motif http://rosalind.info/problems/lcsm/
 #Run with "python -m task_014.not_best_solution"
 import itertools
@@ -9,16 +10,16 @@ def make_subs(sequence):
     last_position = len(sequence)
     substrings = []
     for i in range(last_position):
-        substrings.append([i, last_position - i])
+        substrings.append((i, last_position))
     return substrings
 
  
 def find_longes_prefix(needle, needle_positions, haystack, begin_haystack, len_haystack):
-    for i, j in zip(range(needle_positions[0], needle_positions[0] + needle_positions[1]), 
+    for i, j in zip(range(needle_positions[0], needle_positions[1]), 
                     range(begin_haystack, len_haystack)):
         if needle[i] != haystack[j]:
-            return i - needle_positions[0]
-    return i - needle_positions[0] + 1
+            return i
+    return i + 1
 
 
 def strings_comparison(needle, needle_positions, haystack):
@@ -26,12 +27,14 @@ def strings_comparison(needle, needle_positions, haystack):
     len_haystack = len(haystack)
 
     for begin_haystack in range(len_haystack):
-        length = find_longes_prefix(needle, 
-                                    needle_positions, 
-                                    haystack, 
-                                    begin_haystack, 
-                                    len_haystack)
-        result = max(result, length)
+        needle_end = find_longes_prefix(needle, 
+                                        needle_positions, 
+                                        haystack, 
+                                        begin_haystack, 
+                                        len_haystack)
+        result = max(result, needle_end)
+        if result == needle_positions[1]:
+            break
 
     return result
 
@@ -47,11 +50,12 @@ def main():
 
     for i, string in enumerate(sequences):
         if i > 1:
-            for needle_positions in substrings:
-                needle_positions[1] = strings_comparison(needle, needle_positions, string)
+            for j, needle_positions in enumerate(substrings):
+                substring_end = strings_comparison(needle, needle_positions, string)
+                substrings[j] = (needle_positions[0], substring_end)
     
-    position = max(substrings, key=lambda x: x[1])
-    print(sequences[0][position[0]:position[0]+position[1]])
+    position = max(substrings, key=lambda x: x[1] - x[0])
+    print(sequences[0][position[0]:position[1]])
        
 
 if __name__ == '__main__':
--- task_014/test_notbest.py
+++ task_014/test_notbest.py
@@ -5,16 +5,16 @@ from .not_best_solution import make_subs, find_longes_prefix, strings_comparison
 
 
 def test_make_subs():
-    assert make_subs('ACGT') == [[0, 4],[1, 3],[2, 2],[3, 1]]
+    assert make_subs('ACGT') == [(0, 4),(1, 4),(2, 4),(3, 4)]
 
 
 def test_find_longes_prefix():
-    assert find_longes_prefix('ACGT', [1, 3], 'GGGGGG', 0, 6) == 0
+    assert find_longes_prefix('ACGT', [1, 4], 'GGGGGG', 0, 6) == 1
     assert find_longes_prefix('G', [0, 1], 'GAAAAA', 0, 6) == 1
-    assert find_longes_prefix('GDDDA', [4, 1], 'A', 0, 1) == 1
+    assert find_longes_prefix('GDDDA', [4, 5], 'A', 0, 1) == 5
 
 
 def test_strings_comparison():
-    assert strings_comparison('ATACA', [4, 1], 'GATTACA') == 1
+    assert strings_comparison('ATACA', [4, 5], 'GATTACA') == 5
     assert strings_comparison('TTTTT', [0, 1], 'GGGGGG') == 0
     
